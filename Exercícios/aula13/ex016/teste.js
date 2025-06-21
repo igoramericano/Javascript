81f1b250593425af064a520d8cb75ae161eb2b2b
Igor Americano <igoramericano10@gmail.com>
2025-06-21 10:04:38 -0300

    a

--- Exercícios/aula13/ex016/teste.js
+++ Exercícios/aula13/ex016/teste.js
@@ -1,15 +1,33 @@
 var c = 1
+
+while (c <= 10) {
+  console.log(`passo ${c}`)
+  c++
+}
+//fazem a mesma coisa (acima e abaixo)
+for (var c =1; c <=10; c++){
+  console.log(`passo ${c}`)
+  c++
+}
+
+//MODELO FOR (variável, teste lógico, incremento)
+
+/*
+
+var c = 1
 while (c <= 6) {
     console.log (`passo ${c}`)
     c++
 }
 
 
-/*
-  fazem a mesma coisa
-*/
+
+//fazem a mesma coisa (acima e abaixo)
+
 
 do {
     console.log (`passo ${c}`)
     c++
 } while (c <=12)
+
+*/
